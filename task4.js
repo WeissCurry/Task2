07b45ac5a3a6dda64b0f9a813634e73dc93ca0a0
WeissCurry <Maulanasykari@gmail.com>
2023-08-30 21:16:10 +0700

    revisi1

--- task4.js
+++ task4.js
@@ -2,7 +2,12 @@
 1. Mmebuat function calculator
 */
 
-function kalkulator (angka1, angka2) {
+function kalkulator(angka1, angka2) {
+    if (typeof angka1 !== 'number' || typeof angka2 !== 'number') {
+        console.log("Itu bukan angka")
+        return
+    }
+    
     let mauTambah = angka1 + angka2
     let mauKurang = angka1 - angka2
     let mauKali = angka1 * angka2
@@ -10,23 +15,27 @@ function kalkulator (angka1, angka2) {
     let mauPangkat = angka1 ** angka2
     
     return {
-        tambah : mauTambah,
-        kurang : mauKurang,
-        kali : mauKali,
-        bagi : mauBagi,
-        pangkat : mauPangkat,
+        tambah: mauTambah,
+        kurang: mauKurang,
+        kali: mauKali,
+        bagi: mauBagi,
+        pangkat: mauPangkat,
     }
 }
 
 let num1 = 2
 let num2 = 10
-let hasilOperasi = kalkulator(num1, num2)
+let hasil = kalkulator(num1, num2)
 
-console.log("Hasil dari : " + num1 + " + " + num2 + " = " +  hasilOperasi.tambah)
-console.log("Hasil dari : " + num1 + " - " + num2 + " = " + hasilOperasi.kurang)
-console.log("Hasil dari : " + num1+ " x " + num2 + " = " + hasilOperasi.kali)
-console.log("Hasil dari : " + num1+ " / " + num2 + " = " + hasilOperasi.bagi)
-console.log("Hasil dari : " + num1+ " ^ " + num2 + " = " + hasilOperasi.pangkat)
+if (hasil) {
+    console.log("Hasil dari : " + num1 + " + " + num2 + " = " +  hasil.tambah)
+    console.log("Hasil dari : " + num1 + " - " + num2 + " = " + hasil.kurang)
+    console.log("Hasil dari : " + num1 + " x " + num2 + " = " + hasil.kali)
+    console.log("Hasil dari : " + num1 + " / " + num2 + " = " + hasil.bagi)
+    console.log("Hasil dari : " + num1 + " ^ " + num2 + " = " + hasil.pangkat)
+} else {
+    console.log("Tidak dapat melakukan  kalkulator")
+}
 
 /*
 2. Buatkan soal sebelumnya dengan function declaration dan expression
